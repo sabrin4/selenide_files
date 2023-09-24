8e8072fc84cdcb8266ccf39f3ab3eb87d3f8f225
sabrin4 <sabrinq4@gmail.com>
2023-09-24 15:45:12 +0400

    delete useless comments

--- src/test/java/parsing/Person.java
+++ src/test/java/parsing/Person.java
@@ -2,17 +2,6 @@ package parsing;
 
 import java.util.List;
 
-//{
-//        "name": "Sergey",
-//        "hasJobOffer": false,
-//        "age": "35",
-//        "languages": ["english", "russian"],
-//        "address": {
-//        "city": "Tarkov",
-//        "street": "Chekannaya",
-//        "building": 12
-//        }
-//        }
 public class Person {
     public String name;
     public Boolean hasJobOffer;
